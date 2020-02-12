e1a3156d32e9df30d28cb7e22231a9e5e0b851be
esuder <esuder@users.noreply.github.com>
2020-02-12 16:49:05 -0500

    modified part 1

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -2,7 +2,9 @@ import cc_dat_utils
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
-cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+
+mylevel = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+print(mylevel)
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
